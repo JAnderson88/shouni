1da93893f2d5d9c8c86cb9a9ccd9b5de82e17385
Jeremy Anderson <jande081@fiu.edu>
2016-07-12 10:29:11 -0400

    Added javascript functionality for various divs on search page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,6 +3,14 @@ $(".home.index").ready(function(){
 });
 
 $(".home.search").ready(function(){
-	
-});
+	fadeTitle();
+})(jQuery);
+
+function fadeTitle(){
+	$(".home.search .title").fadeOut(4000, function(){
+		$(this).mouseenter(function(){
+			$(this).fadeIn(10);
+		});
+	});
+}
 
